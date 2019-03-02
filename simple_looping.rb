574aab29801a9acbbf29c38172bc1a8e1d420584
Preston Elliott <preston.elliott24@gmail.com>
2019-03-02 15:52:06 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -55,7 +55,7 @@ end
 # remember that `for` requires a range of numbers. How can we turn our number into a range?
 
 def for_iterator(number_of_times)
-  for number_of_times 0..6 do
+  for number_of_times 0 ..6 do
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
   end
