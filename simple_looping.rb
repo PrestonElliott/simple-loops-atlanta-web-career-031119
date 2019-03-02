84216a6b49bc61d3d6cef5904f40405fe990c3a9
Preston Elliott <preston.elliott24@gmail.com>
2019-03-02 15:45:58 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,3 +1,10 @@
+# Code your solution here using the "loop" keyword to puts out the below phrase
+# This and all the other methods take an argument of an integer 
+# The integer is the number of times the loops should puts out the phrase
+# How can we make sure the loop breaks once it has puts out the phrase the 
+#   correct number of times?
+# Maybe we should keep count of the number of times we've puts out the 
+#  phrase and break when the counter hits the appropriate number...
 # REMEMBER: print your output to the terminal using 'puts'
 def loop_iterator(number_of_times)
   number_of_times = 0
@@ -11,34 +18,35 @@ def loop_iterator(number_of_times)
   end
 end
     
-  # Code your solution here using the "loop" keyword to puts out the below phrase
-  # This and all the other methods take an argument of an integer 
-  # The integer is the number of times the loops should puts out the phrase
-  # How can we make sure the loop breaks once it has puts out the phrase the 
-  #   correct number of times?
-  # Maybe we should keep count of the number of times we've puts out the 
-  #  phrase and break when the counter hits the appropriate number...
-
+# code your solution here using the "times" keyword
 def times_iterator(number_of_times)
   7.times do 
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
-  # code your solution here using the "times" keyword
   end
 end
 
+# code your solution here using the "while" keyword
+# hint: user a counter to tell the while loop when to stop!
+
 def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  number_of_times = 0 
+  while number_of_times <= 7
+    number_of_times += 1 
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+  end
 end
 
-def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  
+def until_iterator(number_of_times)
+  number_of_times = 0 
+  until number_of_times == 7
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+  end
 end
 
 def for_iterator(number_of_times)
